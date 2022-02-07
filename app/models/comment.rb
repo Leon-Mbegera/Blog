29cf1b6d6bbe77b71c973e405647bae7430e28a8
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-02-07 12:46:01 +0300

    Create a concern module to avoid duplication in model files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
+
+  validates :commentor, presence: true
+  validates :body, presence: true, length: { maximum: 15 }
 end
